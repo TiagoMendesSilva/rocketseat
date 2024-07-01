754ca47d6312053a0d2e80c82a589979ab4f4128
TiagoMendesSilva <73915473+TiagoMendesSilva@users.noreply.github.com>
2024-07-01 12:20:08 -0300

    feat: adds logic choose messages
    adds logic to randomly choose within the array and add event when pressing the enter key

--- explorer/stage-5/fortune-cookie/main.js
+++ explorer/stage-5/fortune-cookie/main.js
@@ -2,15 +2,34 @@
 const screen1 = document.querySelector(".screen1");
 const screen2 = document.querySelector(".screen2");
 
+/*eventos*/
 const imgCookieClick = document.querySelector("#imgCookieClick")
 const imgCookieOpenedClick = document.querySelector("#imgCookieOpenedClick")
 
+const fortune = [
+    "O aprendizado é como o horizonte: não há limites.",
+    "Não há que ser forte, há que ser flexível.",
+    "Limitações são fronteiras criadas apenas pela nossa mente.",
+    "O cão não ladra por valentia e sim por medo.",
+    "Procure acender uma vela em vez de amaldiçoar a escuridão.",
+    "A palavra é prata, o silêncio é ouro.",
+    "Lembre-se de que grandes realizações e grandes amores envolvem grandes riscos.",
+    "Um pouco de perfume sempre fica nas mãos de quem oferece flores.",
+    "O homem só envelhece quando os lamentos substituem seus sonhos.",
+    "A persistência realiza o impossível.",
+    "Se alguém está tão cansado que não possa te dar um sorriso, deixa-lhe o teu.",
+  ]
+
+
 imgCookieClick.addEventListener('click', handleImgCookieClick)
 imgCookieOpenedClick.addEventListener('click', handleImgCookieOpenedClick)
+document.addEventListener('keydown', handlekeyDown);
+
 
 
 function handleImgCookieClick(){
     toggleScreen()
+    pickFortune()
 }
 
 function handleImgCookieOpenedClick(){
@@ -23,4 +42,17 @@ function toggleScreen(){
     
 }
 
+function pickFortune(){
+    let allFortunes = fortune.length
+    let randomNumberMessage = Math.floor(Math.random()*allFortunes);
+    screen2.querySelector("p").innerText = `${fortune[randomNumberMessage]}`
+}
+
+function handlekeyDown(event){
+    if(event.key == 'Enter' && screen1.classList.contains('hide')){
+        handleImgCookieOpenedClick()
+    } else if( event.key == 'Enter' && screen2.classList.contains('hide')){
+        handleImgCookieClick()
+    }
+}
 
